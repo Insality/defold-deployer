6e8c7bf7830b63e2ff013b9e93b491b41ef08833
Insality <insality@gmail.com>
2021-12-13 17:45:25 +0100

    #17 add --no-resolve instead of --resolve (resolve by default)

--- deployer.sh
+++ deployer.sh
@@ -10,7 +10,7 @@
 ## See full instructions here: https://github.com/Insality/defold-deployer/blob/master/README.md
 ##
 ## Usage:
-## bash deployer.sh [a][i][h][w][l][m][r][b][d] [--fast] [--resolve] [--instant] [--settings {filename}] [--headless] [--param {x}]
+## bash deployer.sh [a][i][h][w][l][m][r][b][d] [--fast] [--no-resolve] [--instant] [--settings {filename}] [--headless] [--param {x}]
 ## 	a - add target platform Android
 ## 	i - add target platform iOS
 ## 	h - add target platform HTML5
@@ -21,8 +21,8 @@
 ## 	b - build project (game bundle will be in ./dist folder)
 ## 	d - deploy bundle to connected device
 ## 		it will deploy && run bundle on Android/iOS with reading logs to terminal
-## 	--fast - only one Android platform (for faster builds)
-## 	--resolve - build with dependency resolve
+## 	--fast - only one Android platform, without resolve (for faster builds)
+## 	--no-resolve - build without dependency resolve
 ## 	--headless - set mode to headless. Override release mode
 ## 	--settings {filename} - add settings file to build params. Can be used several times
 ## 	--param {x} - add flag {x} to bob.jar. Can be used several times
@@ -494,7 +494,7 @@ is_html=false
 is_linux=false
 is_macos=false
 is_windows=false
-is_resolve=false
+is_resolve=true
 is_android_instant=false
 is_fast_debug=false
 mode="debug"
@@ -549,8 +549,8 @@ do
 			is_fast_debug=true
 			shift
 		;;
-		--resolve)
-			is_resolve=true
+		--no-resolve)
+			is_resolve=false
 			shift
 		;;
 		--settings)
